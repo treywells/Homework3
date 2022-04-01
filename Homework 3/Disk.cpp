16b0908b5e9f7672b2099d1f909cc22b267b804a
Trey <wells.t.2020@gmail.com>
2022-04-01 16:28:40 -0500

    Creation of Buffer Added to Disk Constructor

--- Homework 3/Disk.cpp
+++ Homework 3/Disk.cpp
@@ -1,8 +1,6 @@
 #include "pch.h"
 
-Disk::Disk(HANDLE eventQuit, PC* pcEmpty, PC* pcFull, DWORD bufSize, char* fileName) : eventQuit(eventQuit), pcEmpty(pcEmpty), pcFull(pcFull), bufSize(bufSize) {
-
-	buf = new char[bufSize];
+Disk::Disk(DWORD threads, DWORD longestWord, char* fileName) {
 
 	if ((file = CreateFile(fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == NULL) {
 		printf("Opening %s failed\n", fileName);
@@ -10,6 +8,20 @@ Disk::Disk(HANDLE eventQuit, PC* pcEmpty, PC* pcFull, DWORD bufSize, char* fileN
 	}
 
 	amountRead = 0;
+
+    DWORD sectorSize;
+
+    GetDiskFreeSpace(NULL, NULL, &sectorSize, NULL, NULL);
+    // align max string length to sector size, which gives us the first shadow buffer length;
+    // the second shadow buffer only holds a NULL and can be limited to just sectorSize
+    int shadowSize = (longestWord / sectorSize + 1) * sectorSize;
+    int nSlots = threads; // how many slots to maintain
+    int padding = shadowSize + sectorSize; // both shadow buffers
+    int b = 1 << 20; // 1MB in each slot
+    int slotSize = b + padding; // full slot with padding
+    // VirtualAlloc guarantees page-aligned addresses, while the heap does not
+    buf = (char*)VirtualAlloc(NULL, (UINT64)nSlots * slotSize,
+        MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
 }
 
--- Homework 3/Disk.h
+++ Homework 3/Disk.h
@@ -6,10 +6,9 @@ class Disk {
 	PC* pcEmpty;
 	PC* pcFull;
 	char* buf;
-	DWORD bufSize;
 	DWORD amountRead;
 
 public:
-	Disk(HANDLE eventQuit, PC* pcEmpty, PC* pcFull, DWORD bufSize, char* fileName);
+	Disk(DWORD threads, DWORD longestWord, char* fileName);
 	~Disk();
 };
