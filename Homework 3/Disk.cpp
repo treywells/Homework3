d7a712d55f36b6e5b5294e647f1789b025970c2a
Trey <wells.t.2020@gmail.com>
2022-04-01 15:28:49 -0500

    Created Parameterized Constructor

--- Homework 3/Disk.cpp
+++ Homework 3/Disk.cpp
@@ -1,2 +1,20 @@
 #include "pch.h"
 
+Disk::Disk(HANDLE eventQuit, PC* pcEmpty, PC* pcFull, DWORD bufSize, char* fileName) : eventQuit(eventQuit), pcEmpty(pcEmpty), pcFull(pcFull), bufSize(bufSize) {
+
+	buf = new char[bufSize];
+
+	if ((file = CreateFile(fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == NULL) {
+		printf("Opening %s failed\n", fileName);
+		exit(-1);
+	}
+
+	amountRead = 0;
+
+}
+
+Disk::~Disk() {
+	delete[] buf;
+}
+
+
--- Homework 3/Disk.h
+++ Homework 3/Disk.h
@@ -1,5 +1,15 @@
 #pragma once
 
 class Disk {
+	HANDLE file;
+	HANDLE eventQuit;
+	PC* pcEmpty;
+	PC* pcFull;
+	char* buf;
+	DWORD bufSize;
+	DWORD amountRead;
 
+public:
+	Disk(HANDLE eventQuit, PC* pcEmpty, PC* pcFull, DWORD bufSize, char* fileName);
+	~Disk();
 };
