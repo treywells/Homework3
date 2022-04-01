8d9fb45cab2b40a00200cf38127e90ecae38655e
Trey <wells.t.2020@gmail.com>
2022-04-01 11:40:54 -0500

    PC without Synchronization Finalized. Prior to Extensive Testing

--- Homework 3/PC.cpp
+++ Homework 3/PC.cpp
@@ -1,1 +1,42 @@
 #include "pch.h"
+
+PC::PC(HANDLE eventQuit, DWORD slots, DWORD sizeOfObject) : eventQuit(eventQuit), slots(slots), sizeOfObject(sizeOfObject) {
+
+	if ((mutex = CreateMutex(NULL,false, NULL)) == NULL) {
+		printf("Creation the Mutex failed\n");
+		exit(-1);
+	}
+
+	if ((semaFullSlots = CreateSemaphore(NULL, 0, slots, NULL)) == NULL) {
+		printf("Creation the semaFullSlots failed\n");
+		exit(-1);
+	}
+
+	if ((semaEmptySlots = CreateSemaphore(NULL, slots, slots, NULL)) == NULL) {
+		printf("Creation the semaFullSlots failed\n");
+		exit(-1);
+	}
+
+	allocatedSpace = slots * sizeOfObject;
+	buf = new char[allocatedSpace];
+	tail = 0;
+	head = 0;
+	size = 0;
+
+}
+
+void PC::Produce(char* source) {
+
+	memcpy(buf + tail, source, sizeOfObject);
+	tail = (tail + sizeOfObject) % allocatedSpace;
+	size++;
+
+}
+
+void PC::Consume(char* destination) {
+
+	memcpy(destination, buf + head, sizeOfObject);
+	head = (head + sizeOfObject) % allocatedSpace;
+	size++;
+
+}
--- Homework 3/PC.h
+++ Homework 3/PC.h
@@ -1,5 +1,20 @@
 #pragma once
 
 class PC {
+	char* buf;
+	DWORD head;
+	DWORD tail;
+	DWORD sizeOfObject;
+	DWORD size;
+	DWORD slots;
+	DWORD allocatedSpace;
+	HANDLE eventQuit;
+	HANDLE semaFullSlots;
+	HANDLE semaEmptySlots;
+	HANDLE mutex;
 
+public:
+	PC(HANDLE eventQuit, DWORD slots, DWORD sizeOfObject);
+	void Produce(char* source);
+	void Consume(char* destination);
 };
