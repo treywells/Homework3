a7fab7673443f7395cc3c3a11e23d02333c468f5
Trey <wells.t.2020@gmail.com>
2022-04-02 00:08:37 -0500

    Run Function Initial Finish

--- Homework 3/Disk.cpp
+++ Homework 3/Disk.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 
-Disk::Disk(DWORD threads, DWORD longestWord, char* fileName) {
+Disk::Disk(DWORD slots, DWORD longestWord, char* fileName) : slots(slots) {
 
 	if ((file = CreateFile(fileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == NULL) {
 		printf("Opening %s failed\n", fileName);
@@ -14,14 +14,46 @@ Disk::Disk(DWORD threads, DWORD longestWord, char* fileName) {
     GetDiskFreeSpace(NULL, NULL, &sectorSize, NULL, NULL);
     // align max string length to sector size, which gives us the first shadow buffer length;
     // the second shadow buffer only holds a NULL and can be limited to just sectorSize
-    int shadowSize = (longestWord / sectorSize + 1) * sectorSize;
-    int nSlots = threads; // how many slots to maintain
+    shadowSize = (longestWord / sectorSize + 1) * sectorSize;
+    int nSlots = slots; // how many slots to maintain
     int padding = shadowSize + sectorSize; // both shadow buffers
-    int b = 1 << 20; // 1MB in each slot
-    int slotSize = b + padding; // full slot with padding
+    b = 1 << 20; // 1MB in each slot
+    slotSize = b + padding; // full slot with padding
     // VirtualAlloc guarantees page-aligned addresses, while the heap does not
     buf = (char*)VirtualAlloc(NULL, (UINT64)nSlots * slotSize,
         MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+}
+
+void Disk::Run() {
+
+    DWORD bytesRead;
+
+    for (DWORD i = 0; i < slots; i++) {
+        pcEmpty->Produce((char*)&i);
+    }
+
+    do {
+
+        int slotID;
+
+        if (pcEmpty->Consume((char*)&slotID) == QUIT) return;
+
+        if (!ReadFile(file, buf + slotID * slotSize + shadowSize, b, &bytesRead, NULL)) {
+            printf("Error reading wiki file\n");
+            exit(-1);
+        }
+
+        buf[slotID * slotSize + shadowSize + bytesRead] = NULL;
+
+        SearchBuffer searchBuf;
+        searchBuf.ptr = buf + slotID * slotSize + shadowSize;
+        searchBuf.size = bytesRead;
+        searchBuf.slotID = slotID;
+
+        if (pcFull->Produce((char*)&searchBuf) == QUIT) return;
+
+    } while (bytesRead > 0);
 
 }
 
--- Homework 3/Disk.h
+++ Homework 3/Disk.h
@@ -1,14 +1,19 @@
 #pragma once
 
 class Disk {
+public:
 	HANDLE file;
 	HANDLE eventQuit;
 	PC* pcEmpty;
 	PC* pcFull;
 	char* buf;
+	DWORD slots;
+	DWORD shadowSize;
+	DWORD slotSize;
+	DWORD b;
 	DWORD amountRead;
 
-public:
-	Disk(DWORD threads, DWORD longestWord, char* fileName);
+	Disk(DWORD slots, DWORD longestWord, char* fileName);
+	void Run();
 	~Disk();
 };
